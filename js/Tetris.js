88de4ff1e10017c38a3efeb4b0a447c7fd5751be
UncleYee <uncleye159@gmail.com>
2016-12-11 12:51:37 +0800

    增加三个 div 存放基本模块，并且控制四个 div 移动

--- js/Tetris.js
+++ js/Tetris.js
@@ -3,31 +3,35 @@ const move = 20;
 
 // 监听键盘事件 上下左右
 document.onkeydown = (event) => {
-  const model = document.getElementsByClassName("defaultModel")[0];
-  const top = parseInt(model.style.top || 0, 10);
-  console.log(top);
-  const left = parseInt(model.style.left || 0, 10);
-  console.log(left);
-  console.log(model);
-  if (event) {
-    switch(event.keyCode){
-      case 37: { // 左
-        if (left >= 20) {
-          model.style.left = parseInt(left - move, 10) + 'px';
-        }
-      }; break;
-      case 38: { // 上
-        if ( top >= 20) {
-          model.style.top = parseInt(top - move, 10) + 'px';
-        }
-      }; break;
-      case 39: { // 右
-        model.style.left = parseInt(left + move, 10) + 'px';
-      }; break;
-      case 40: { // 下
-        model.style.top = parseInt(top + move, 10) + 'px';
-      }; break;
-      default: break;
+  const models = document.getElementsByClassName("defaultModel");
+  const len = models.length;
+  for (let i = 0; i < len; i++) {
+    const model = models[i];
+    const top = parseInt(model.style.top || 0, 10);
+    console.log(top);
+    const left = parseInt(model.style.left || 0, 10);
+    console.log(left);
+    console.log(model);
+    if (event) {
+      switch(event.keyCode){
+        case 37: { // 左
+          if (left >= 20) {
+            model.style.left = parseInt(left - move, 10) + 'px';
+          }
+        }; break;
+        case 38: { // 上
+          if ( top >= 20) {
+            model.style.top = parseInt(top - move, 10) + 'px';
+          }
+        }; break;
+        case 39: { // 右
+          model.style.left = parseInt(left + move, 10) + 'px';
+        }; break;
+        case 40: { // 下
+          model.style.top = parseInt(top + move, 10) + 'px';
+        }; break;
+        default: break;
+      }
     }
   }
 }
