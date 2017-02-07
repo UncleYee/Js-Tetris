b065caf6b165d934a78a825ff82369a738e42901
UncleYee <uncleye159@gmail.com>
2017-02-07 14:09:52 +0800

    实现消除指定行 实现基本的游戏过程，可以消除填满的一行

--- src/js/Tetris.js
+++ src/js/Tetris.js
@@ -1,5 +1,6 @@
+'use strict';
 // 所有形状
-const SHAPES = [
+let SHAPES = [
   [0,1,1,1,2,1,3,1],
   [1,0,1,1,1,2,2,2],
   [2,0,2,1,2,2,1,2],
@@ -11,32 +12,32 @@ const SHAPES = [
 // 定义坐标
 let x = 0, y = 0;
 // 定义移动的基本单位
-const size = 20;
+let size = 20;
 // 定义形状
-const shape = [];
-const shapeDiv = [];
-const container = {};
+let shape = [];
+let shapeDiv = [];
+let container = {};
 // 定义边界
-const rowCount = 18;
-const colCount = 10;
+let rowCount = 18;
+let colCount = 10;
 
 let defaultInterval; // 默认下降的 interval
 let quickInterval; // 快速下降的 interval
-const speed = 600; // 自动下降速度
+let speed = 600; // 自动下降速度
 
 // 显示方块
-const show = () => {
-  const divs = document.getElementsByClassName("defaultModel");
-  for (let i = 0; i <divs.length; i++) {
-    const top = (shape[i * 2 + 1] + y) * size;
-    const left = (shape[i * 2] + x) * size;
+let show = () => {
+  let divs = document.getElementsByClassName("defaultModel");
+  for (let i = 0; i < divs.length; i++) {
+    let top = (shape[i * 2 + 1] + y) * size;
+    let left = (shape[i * 2] + x) * size;
     divs[i].style.top = `${top}px`;
     divs[i].style.left = `${left}px`;
   }
 }
 
 // 移动
-const move = (a, b)=> {
+let move = (a, b)=> {
   if (check(x + a, y + b, shape)) {
     x += a;
     y += b;
@@ -51,66 +52,111 @@ const move = (a, b)=> {
 }
 
 // 旋转
-const rotate = () => {
-  const newShape = [shape[1], 3 - shape[0], shape[3], 3 - shape[2], shape[5], 3 - shape[4], shape[7], 3 - shape[6]];
+let rotate = () => {
+  let newShape = [shape[1], 3 - shape[0], shape[3], 3 - shape[2], shape[5], 3 - shape[4], shape[7], 3 - shape[6]];
   if(!check(x,y,newShape)) {return;}
   shape = newShape;
   show();
 }
 
 // 边界检查
-const check = (x, y, shape) => {
+let check = (x, y, shape) => {
+  let divs = document.getElementsByClassName('defaultModel');
+
   let left = colCount;
   let right = 0;
   let top = rowCount;
   let bottom = 0;
+  let overlap = false;
 
-  for (let i = 0; i < 8; i += 2) {
+  for (let i = divs.length - 1; i >= 0; i--) {
     // 最左边的水平坐标
     if (shape[i] < left ) { left = shape[i];}
+    // 最右边的水平坐标
     if (shape[i] > right) { right = shape[i];}
+    // 最上边的水平坐标
     if (shape[i + 1] < top) {top = shape[i + 1];}
+    // 最下边的水平坐标
     if (shape[i + 1] > bottom) {bottom = shape[i + 1];}
+    // 判断方块之间是否重叠
+    let px = shape[2 * i + 1] + y;
+    let py = shape[2 * i] + x;
+    if (container[`${px}_${py}`]) {
+      overlap = true;
+    }
   }
 
   if ((right + x + 1) > colCount || (left + x) < 0 ||
-      (bottom + y + 1) > rowCount || (top + y) < 0) {
+      (bottom + y + 1) > rowCount || (top + y) < 0 || overlap) {
         return false;
       }
   return true;
 }
 
 // 快速下降
-const quickDown = () => {
+let quickDown = () => {
   quickInterval = setInterval("move(0,1)", 0);
 }
 
 // 到达底部 固定方块 编程灰色
-const fix = () => {
-  const divs = document.getElementsByClassName('defaultModel');
+let fix = () => {
+  let divs = document.getElementsByClassName('defaultModel');
   for (let i = divs.length - 1; i >= 0; i--) {
-    const px = shape[2 * i + 1] + y;
-    const py = shape[2 * 1] + x;
+    let px = shape[2 * i + 1] + y;
+    let py = shape[2 * 1] + x;
     container[`${px}_${py}`] = shapeDiv[i];
     divs[i].className = 'fixedModel';
   }
-  x = 3;
-  y = 0;
+  findFull();
+}
+
+//检查是否可以消除
+let findFull = () => {
+  let s = 0;
+  for (let i = 0; i < rowCount; i++) {
+    let count = 0;
+    for (let j = 0; j < colCount; j++) {
+      if (container[`${i}_${j}`]) {
+        count++;
+      }
+    }
+    if (count === colCount) {
+      s++;
+      removeLine(m);
+    }
+  }
+}
+
+// 消除指定行
+let removeLine = (row) => {
+  // 消除一行
+  for (let i = 0; i < colCount; i++) {
+    document.body.removeChild(container[`${row}_${i}`]);
+  }
+  // 消除行上面的所有方块向下移动一行
+  for (let i = row; i > 0; i--) {
+    for (let j = 0; j < colCount; j++) {
+      container[`${i}_${j}`] = container[`${i-1}_${j}`];
+      if (container[`${i}_${j}`]) {
+        container[`${i}_${j}`].style.top = `${i * size}px`
+      }
+    }
+  }
 }
 
 // 随机获取一个方块数组
-const randomShape = () => {
-  shape = SHAPES[Math.random() * 7];
+let randomShape = () => {
+  shape = SHAPES[Math.floor(Math.random() * 7)];
 }
 
-const create = () => {
+let create = () => {
   x = 3;
   y = 0;
   shapeDiv = [];
   randomShape();
   // 创建四个 div
   for (let i = 0; i < 4; i++) {
-    const div = document.createElement("div");
+    let div = document.createElement("div");
     div.className = "defaultModel";
     shapeDiv[i] = div;
     document.body.appendChild(div);
@@ -118,15 +164,8 @@ const create = () => {
 }
 
 // 初始化
-const init = () => {
-  // 创建游戏区域
-  const back = document.createElement("div");
-  back.className = "gameBack";
-  document.body.appendChild(back);
-
-
+let init = () => {
   create();
-
   show();
   
   // 监听键盘事件 上下左右
